46aa838eb3ed3643132f5d4fe4d57caf051d6737
SKULSHADY <anushekprasal@gmail.com>
2019-10-07 19:28:33 +0530

    base: Add charging Animation [1/2]

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -1916,9 +1916,7 @@ public final class PowerManagerService extends SystemService
                 if (mBootCompleted) {
                     if (mIsPowered && !BatteryManager.isPlugWired(oldPlugType)
                             && BatteryManager.isPlugWired(mPlugType)) {
-                        mNotifier.onWiredChargingStarted(mForegroundProfile);
-                    } else if (wasPowered && !mIsPowered && !dockedOnWirelessCharger) {
-                        mNotifier.onWiredChargingDisconnected(mForegroundProfile);
+                        mNotifier.onWiredChargingStarted(mBatteryLevel, mForegroundProfile);
                     } else if (dockedOnWirelessCharger) {
                         mNotifier.onWirelessChargingStarted(mBatteryLevel, mForegroundProfile);
                     }
